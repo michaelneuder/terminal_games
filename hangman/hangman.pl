d7b9dc543cce71aa5ec171b9a957d730d129a486
Michael Neuder <michael.neuder@gmail.com>
2017-06-26 18:19:48 -0600

    adding lots of hangman stuff

--- hangman/hangman.pl
+++ hangman/hangman.pl
@@ -6,6 +6,7 @@ use IO::File;
 
 initscr();
 getmaxyx(my $row, my $col);
+my %dictionary;
 
 sub display_title {
   my $col_title = $col/2-30;
@@ -17,6 +18,11 @@ sub display_title {
   addstr(7,$col_title,"| (   ) || (   ) || | \\   || | \\_  )| |   | || (   ) || | \\   |");
   addstr(8,$col_title,"| )   ( || )   ( || )  \\  || (___) || )   ( || )   ( || )  \\  |");
   addstr(9,$col_title,"|/     \\||/     \\||/    )_)(_______)|/     \\||/     \\||/    )_)");
+}
+
+sub populate_dict {
+  %dictionary = (
+    "a"  => " __ _ \n/ _` |\n\\__,_|\n",);
 }
 
 sub get_rand_word {
@@ -33,19 +39,124 @@ sub get_rand_word {
 }
 
 sub display_underscores {
-  my $word = get_rand_word();
+  my $word = $_[0];
   chomp $word;
   my $word_length = length($word);
+  move($row-1, 0);
+  clrtoeol();
   addstr($row-1, 0, "your word has ".$word_length." letters");
   my $display_col = $col/2 - ($word_length*9)/2;
+  move($row-7, 0);
+  clrtoeol();
   for(my $i=0; $i<$word_length; $i++){
-    addstr(20, $display_col+(9*$i), "________  ");
+    addstr($row-7, $display_col+(9*$i), "________  ");
   }
 }
 
-display_title();
-noecho();
-display_underscores();
+sub display_gallows {
+  addstr(14, $col/2-5, "  ---------|       ");
+  addstr(15, $col/2-5, "  |        |       ");
+  addstr(16, $col/2-5, "           |       ");
+  addstr(17, $col/2-5, "           |       ");
+  addstr(18, $col/2-5, "           |       ");
+  addstr(19, $col/2-5, "           |       ");
+  addstr(20, $col/2-5, "           |       ");
+  addstr(21, $col/2-5, "   ________|_______");
+}
 
-getch();
+sub add_head {
+  move(16, $col/2-5);
+  clrtoeol();
+  addstr(16, $col/2-5, "  O        |       ");
+}
+
+sub add_neck {
+  move(17, $col/2-5);
+  clrtoeol();
+  addstr(17, $col/2-5, "  |        |       ");
+}
+
+sub add_left_arm {
+  move(17, $col/2-5);
+  clrtoeol();
+  addstr(17, $col/2-5, "\\ |        |       ");
+}
+
+sub add_right_arm {
+  move(17, $col/2-5);
+  clrtoeol();
+  addstr(17, $col/2-5, "\\ | /      |       ");
+}
+
+sub add_body {
+  move(18, $col/2-5);
+  clrtoeol();
+  addstr(18, $col/2-5, "  |        |       ");
+}
+
+sub add_left_leg {
+  move(19, $col/2-5);
+  clrtoeol();
+  addstr(19, $col/2-5, " /         |       ");
+}
+
+sub add_right_leg {
+  move(19, $col/2-5);
+  clrtoeol();
+  addstr(19, $col/2-5, " / \\       |       ");
+}
+
+sub add_legend {
+  addstr(0,1,'--------------');
+  addstr(1,1,'| qq - quit  |');
+  addstr(2,1,'| rr - reset |');
+  addstr(3,1,'--------------');
+}
+
+sub play {
+  display_title();
+  my $current_word = get_rand_word();
+  display_underscores($current_word);
+  populate_dict();
+  display_gallows();
+  add_legend();
+
+  my $input = '';
+  my $guess_count = 0;
+  while($input ne 'qq'){
+    move($row-2, $col/2);
+    clrtoeol();
+    addstr($row-2, $col/2-10, "enter your guess: ");
+    getstr($input);
+    chomp $input;
+    if($input eq 'rr'){
+      play();
+    } elsif($input eq 'qq'){
+      endwin();
+      exit;
+    } else {
+      for
+    }
+  }
+}
+
+play();
 endwin();
+
+#### useful stuff
+# display_gallows();
+# getch();
+# add_head();
+# getch();
+# add_neck();
+# getch();
+# add_left_arm();
+# getch();
+# add_right_arm();
+# getch();
+# add_body();
+# getch();
+# add_left_leg();
+# getch();
+# add_right_leg();
+# getch();
